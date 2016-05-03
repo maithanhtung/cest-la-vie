48312d57d537d386ece5cc02db056443385d0041
emxin300 <tung4m@gmail.com>
2016-05-03 09:27:04 +0700

    center button in index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,26 @@
 <body>
 		<h1><center>C'EST LA VIE</center></h1>
 		<h3><center>You are...?</center></h3>
+
+		<div class="button">
 		<button><a href="{{url('/sinhvien/dashboard')}}">Sinh vien</a></center></button>
-		<br>
 		<button><a href="{{url('/quanly/dashboard')}}">Quan ly</a></button>
-		<br>
 		<button><a href="{{url('/giaovien/dashboard')}}">Giao vien</a></button>
+		</div>
 </body>
+
+<style type="text/css">
+	
+
+
+div.button{
+	text-align: center;
+	margin :auto;
+}
+
+div.button button{
+	margin :10px;
+	display: inline-block;
+}
+</style>
 </html>
